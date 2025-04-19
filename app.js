eb89c7c594980b0dc447890d4fbe877229576b12
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-04-19 21:12:06 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = "w"
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Chess Game" })
+})
+
+io.on("connection", function (unique) {
+    console.log("connected");
+    if (!players.white) {
+        players.white = unique.id
+        unique.emit("playerRole", "w");
+    } else if (!players.black) {
+        players.black = unique.id
+        unique.emit("playerRole", "b");
+    } else {
+        unique.emit("spectatorRole");
+    }
+
+    unique.on("disconnect", function () {
+        if (unique.id === players.white) {
+            delete players.white
+        } else if (unique.id === players.black) {
+            delete players.black
+        }
+    })
+    unique.on("move", function (move) {
+        try {
+            if (chess.turn() === "w" && unique.id !== players.white) return;
+            if (chess.turn() === "b" && unique.id !== players.black) return;
+            const result = chess.move(move)
+            if (result) {
+                currentPlayer = chess.turn();
+                io.emit("move", move)
+                io.emit("boardState", chess.fen())
+            } else {
+                console.log("Invalid move : ", move);
+                unique.emit("move", move)
+            }
+        } catch (err) {
+            console.log(err);
+            unique.emit("InvalidMove", move);
+        }
+    })
+})
+
+server.listen(3000, function () {
+    console.log('listening on port "http://localhost:3000"')
+})
